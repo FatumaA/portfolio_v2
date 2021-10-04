755285b324981ad3d29a15a15b7788b4b9d7db29
FatumaA <67555014+FatumaA@users.noreply.github.com>
2021-10-04 09:19:40 +0300

    Config - tailwind, package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,31 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', 
-          './components/**/*.{js,ts,jsx,tsx}'
-          ],
+  purge: ['./src/**/*.{js,ts,jsx,tsx,css}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgColor: {
+          light: '#F4F4F4',
+          dark: '#222'
+        },
+        brand: {
+          primary: '#55266F',
+          hover: '#6B4FC4',
+          accent: '#c7c734',
+          accentGreen: '#41B09C',
+          accentOrange: '#E18888'
+        },
+        textColor: {
+          light: '#fff',
+          dark: '#222'
+        }
+      },
+      text: {
+        light: '#fff'
+      }
+    },
   },
   variants: {
     extend: {},
