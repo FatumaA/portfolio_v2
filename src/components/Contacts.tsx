bb83c5cbf50c3cef8ab3023d4c90508fb46d0ecc
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-01-25 14:14:13 +0300

    fix very light text when typing in the input fields

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -11,17 +11,17 @@ const Contacts = () => {
 
             <p className="text-center leading-7">{ContactInfo.subHeadLine}</p>
 
-            <form name='contact' action='/contact' method='POST' className="flex flex-col w-11/12 text-center my-6">
+            <form name='contact' action='/#contact' method='POST' className="flex flex-col w-11/12 text-center my-6">
                 <input type='hidden' name='form-name' value='contact'/>
 
                 <label htmlFor='Name' className="mb-1">Name:</label>
-                <input className="p-2 rounded-sm" type='text' name='name' placeholder ='Name...' required />
+                <input className="p-2 rounded-sm text-gray-700" type='text' name='name' placeholder ='Name...' required />
 
                 <label htmlFor='Email' className="mt-6 mb-1">Email:</label>
-                <input className="p-2 rounded-sm" type='email' name='email' placeholder='Email...' required />
+                <input className="p-2 rounded-sm text-gray-700" type='email' name='email' placeholder='Email...' required />
   
                 <label htmlFor='Message' className="mt-6 mb-1">Message:</label>
-                <textarea className="p-2 rounded-sm" rows={10} name='message' required placeholder='Type your message here...' />
+                <textarea className="p-2 rounded-sm text-gray-700" rows={10} name='message' required placeholder='Type your message here...' />
 
                 <Button btnText={ButtonInfo.contact}/>
             </form> 
