0489c4e41a8b94d48a95676077daae513e096dfd
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-01-25 11:36:28 +0300

    give margins

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,7 +4,7 @@ import { BannerInfo } from '../Data'
 
 const Banner = () => {
     return (
-        <div className="text-white border-t-2 border-b-2 border-brand-accent py-10 flex flex-row justify-between">
+        <div className="text-white border-t-2 border-b-2 border-brand-accent py-10 flex flex-row justify-between mx-8">
             { BannerInfo.map((img, index) => (
                 <Image key={index} alt={img} src={img} width={100} height={100}></Image>
             ))
