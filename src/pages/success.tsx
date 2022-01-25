d52d6be0f7ce888a1ad08d47bbec3cd921f5d366
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-01-25 16:46:06 +0300

    prettify success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,9 +1,26 @@
-const success = () => {
-  return <div>
-      Form Submitted Successfully.
-      We will get back to you 
-      Thank you
-  </div>;
-};
+/* eslint-disable @next/next/no-html-link-for-pages */
+import Link from "next/link";
+import Image from 'next/image'
 
-export default success;
+const Success = () => {
+  return (
+    <div className='card text-center top-styles mx-8 sm:mx-14'>
+        <a href='/'>
+            <Image src='/images/sent.png' alt='screenshot' width={180} height={120}/> 
+            <h3 className="my-4 font-extrabold"> 
+                Form submission received 
+            </h3>
+            <p  className="mt-2 mb-4"> 
+                Thank you for your submission. I will get back to you shortly 
+            </p>
+            <Link href='/' passHref> 
+                <span className="text-yellow-400 hover:scale-50"> 
+                    Go Home 
+                </span> 
+            </Link>
+        </a>
+    </div>
+  )
+}
+
+export default Success
