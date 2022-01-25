1ab738ad0c0118a235c177d4e1b7239362d31c11
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-01-25 12:02:42 +0300

    make entire card clickable

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,18 +13,18 @@ type props = {
           projCodeUrl: string; }
   }
 
-function Card(Proj: props ) {
-    return (     
-          <div className='card'>
-            <a href={Proj.Proj.projectImgUrl} target='_blank' rel='noreferrer'>
-            <Image src={Proj.Proj.projImgSrc} alt='screenshot' width="200" height="80"/> 
-            </a>
-            <h3 className="mt-4"> {Proj.Proj.projStack} </h3>
-            <p  className="mt-2 mb-4"> {Proj.Proj.projDesc} </p>
-            <a href={Proj.Proj.projCodeUrl} target='_blank' rel='noreferrer' className="text-yellow-400 hover:scale-50"> 
-            {Proj.Proj.projTextLink} 
-            </a>
-          </div>
+function Card({Proj}: props ) {
+    return (  
+      <div className='card'>
+        <a href={Proj.projectImgUrl} target='_blank' rel='noreferrer'>
+          <Image src={Proj.projImgSrc} alt='screenshot' width="200" height="80"/> 
+          <h3 className="mt-4"> {Proj.projStack} </h3>
+          <p  className="mt-2 mb-4"> {Proj.projDesc} </p>
+          <a href={Proj.projCodeUrl} target='_blank' rel='noreferrer' className="text-yellow-400 hover:scale-50"> 
+            {Proj.projTextLink} 
+          </a>
+        </a>
+      </div>
     )
 }
 
