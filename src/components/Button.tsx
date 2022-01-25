f4b3512e39274b49a6517b0200c5ea3f2b7f7c3a
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-01-25 14:29:28 +0300

    add type to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,11 +3,12 @@ import { IconBaseProps } from 'react-icons'
 type props = {
     btnText: string;
     icon?: IconBaseProps;
+    type?: "button" | "submit" | "reset" | undefined;
 }
 
-const Button = ( { btnText, icon }: props ) => {
+const Button = ( { btnText, icon, type }: props ) => {
     return (
-        <button className="btn-primary flex flex-row justify-center">
+        <button type={type} className="btn-primary flex flex-row justify-center">
              { icon && <div> {icon} </div> }
             <div> {btnText} </div>     
         </button>
