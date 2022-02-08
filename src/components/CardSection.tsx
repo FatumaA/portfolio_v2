c2de5b8c9412099d056fa81107198487868ff6fe
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-02-08 10:13:33 +0300

    update card section with new project info

--- src/components/CardSection.tsx
+++ src/components/CardSection.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Card from './Card'
-import { CodeProjectsInfo, LowCodeProjectsInfo, headAndTagLine } from '../Data'
+import { ProfessionalProjectsInfo, PersonalProjectsInfo, headAndTagLine } from '../Data'
 
 const CardSection = () => {
     return (
@@ -9,18 +9,18 @@ const CardSection = () => {
             <h1 className="headline" >{headAndTagLine.headLine}</h1>
             <div className="card-section">
                 {
-                    CodeProjectsInfo.map((codeProj, index) => (
-                        <Card key={index} Proj={codeProj}/>
+                    ProfessionalProjectsInfo.map((profProj, index) => (
+                        <Card key={index} Proj={profProj}/>
                     ))
                 }
             </div>
 
-            <h5 className="tagline mt-10"> {headAndTagLine.tagLineWP} </h5>
-            <h1 className="headline" >{headAndTagLine.headLineWP}</h1>
-            <div className="card-section md:grid-cols-2">
+            <h5 className="tagline mt-10"> {headAndTagLine.tagLinePP} </h5>
+            <h1 className="headline" >{headAndTagLine.headLinePP}</h1>
+            <div className="card-section">
                 {
-                    LowCodeProjectsInfo.map((lowCodeProj, index) => (
-                        <Card key={index} Proj={lowCodeProj}/>
+                    PersonalProjectsInfo.map((personalProj, index) => (
+                        <Card key={index} Proj={personalProj}/>
                     ))
                 }
             </div>
