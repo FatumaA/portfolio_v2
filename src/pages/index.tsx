a2a712b8d952a28f448a5166cc207073ded972e3
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-08-25 18:22:17 +0300

    remove unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,13 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Button from '../components/Button'
-import { AiOutlineArrowRight } from 'react-icons/ai';
-import Card from '../components/Card';
 import CardSection from '../components/CardSection';
 import Hero from '../components/Hero';
 import React from 'react';
 import Banner from '../components/Banner';
-import Projects from '../components/Projects';
 import About from '../components/About';
 import Contacts from '../components/Contacts';
+
 const Home: NextPage = () => {
- 
-  
   return (
     <div className="max-w-4xl mx-auto w-4/5">
       <Head>
@@ -20,7 +15,6 @@ const Home: NextPage = () => {
         <meta name="description" content="Portfolio version 2" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
 
       {/* <Button btnText="See Projects" icon={<AiOutlineArrowRight style={{ fontWeight: 'bold'}}/>} /> */}
       <main className="relative">
