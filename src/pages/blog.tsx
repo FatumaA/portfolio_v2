7a96031339e5ab004d5f5d1b4cd94367d0b0697a
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-08-25 18:25:51 +0300

    display blog excerpt as cards on blog page 🎉

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import Button from '../components/Button'
+import Card from '../components/Card';
 
-const blog = ({}) => {
+const blog = ({posts}: any) => {
     return (
-        <div className="bg-white">
-            <h1> Hello Blogs </h1>
+        <div className="top-styles my-14">
+            <h5 className="tagline"> BLOG POSTS </h5>
+            <h1 className="headline">Thoughts on the tech I&apos;m using, learning and loving </h1>
+            <div className="card-section">
+            {posts.map((p: any, index: number ) => 
+             <Card key={index} blogs={p}/>
+
+            )}
+
+            </div>
+            
 
         </div>
     )
@@ -15,12 +25,13 @@ export default blog
 
 const GET_BLOGS = `
     {
-        user(username: "catalinpit") {
+        user(username: "HijabiCoder") {
           publication {
             posts(page: 0) {
               title
               brief
               slug
+              coverImage
             }
           }
         }
@@ -37,11 +48,11 @@ export async function getStaticProps() {
       const { data } = await client.query({
         query: gql `${GET_BLOGS}`
       });
-      console.log(data)
+      console.log('DATAAAA', data.user.publication.posts)
 
     return {
         props: {
-
+          posts: data.user.publication.posts
         }
     }  
   }
