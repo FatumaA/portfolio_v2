c75ca4e1a573b58e40923bacb025c8a04ecdb30f
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-08-25 18:23:41 +0300

    modify card to accept both project & blog - prob temp-

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,27 +1,41 @@
 import React from 'react'
 import Image from 'next/image'
 
+interface proj {
+  key?: number;
+  projectImgUrl: string; 
+  projImgSrc: string; 
+  projImgAlt: string;
+  projStack: string; 
+  projDesc: string; 
+  projTextLink: string; 
+  projCodeUrl: string;
+}
 
-type props = {
-  key: number; 
-  Proj: { projectImgUrl: string; 
-          projImgSrc: string; 
-          projImgAlt: string;
-          projStack: string; 
-          projDesc: string; 
-          projTextLink: string; 
-          projCodeUrl: string; }
-  }
+interface blog {
+  key?: number;
+  title: string;
+  brief: string;
+  slug: string;
+  coverImage: string;
+}
 
-function Card({Proj}: props ) {
+type cardInfo = {
+  key: number;
+  projs?: proj;
+  blogs?: blog;
+}
+
+function Card(props : cardInfo) {
+  const isProj = props.projs !== undefined;
     return (  
       <div className='card'>
-        <a href={Proj.projectImgUrl} target='_blank' rel='noreferrer'>
-          <Image src={Proj.projImgSrc} alt='screenshot' width="200" height="80"/> 
-          <h3 className="my-4 font-extrabold"> {Proj.projStack} </h3>
-          <p  className="mt-2 mb-4"> {Proj.projDesc} </p>
-          <a href={Proj.projCodeUrl} target='_blank' rel='noreferrer' className="text-yellow-400 hover:scale-50"> 
-            {Proj.projTextLink} 
+        <a href={isProj ? props.projs!.projectImgUrl : props.blogs!.slug } target='_blank' rel='noreferrer'>
+        <Image src={isProj ? props.projs!.projImgSrc : props.blogs!.coverImage} alt='screenshot' width="200" height="80"/> 
+          <h3 className="my-4 font-extrabold"> {isProj ? props.projs!.projStack : props.blogs?.title} </h3>
+          <p  className="mt-2 mb-4"> {isProj ? props.projs!.projDesc : props.blogs?.brief} </p>
+          <a href={isProj ? props.projs!.projCodeUrl : props.blogs?.slug} target={isProj ? '_blank': undefined} rel='noreferrer' className="text-yellow-400 hover:scale-50"> 
+            {isProj ? props.projs!.projTextLink: "Read More"} 
           </a>
         </a>
       </div>
