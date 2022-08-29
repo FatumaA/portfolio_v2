da50875a09d5a6558d976875d4fd9b46ea20daeb
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-08-29 14:42:04 +0300

    create nested routing for blogs and single blog

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
-import Button from '../components/Button'
-import Card from '../components/Card';
 
-const blog = ({posts}: any) => {
+import { NextPage } from 'next';
+import Card from '../../components/Card';
+import { Client, GET_BLOGS } from '../../queries';
+
+const Blog: NextPage = ({posts}: any) => {
     return (
         <div className="top-styles my-14">
             <h5 className="tagline"> BLOG POSTS </h5>
@@ -21,34 +23,14 @@ const blog = ({posts}: any) => {
     )
 }
 
-export default blog
+export default Blog
 
-const GET_BLOGS = `
-    {
-        user(username: "HijabiCoder") {
-          publication {
-            posts(page: 0) {
-              title
-              brief
-              slug
-              coverImage
-            }
-          }
-        }
-      }
-`;
 
 
 export async function getStaticProps() {
-    const client = new ApolloClient({
-        uri: 'https://api.hashnode.com/',
-        cache: new InMemoryCache()
-      });
-
-      const { data } = await client.query({
+      const { data } = await Client.query({
         query: gql `${GET_BLOGS}`
       });
-      console.log('DATAAAA', data.user.publication.posts)
 
     return {
         props: {
