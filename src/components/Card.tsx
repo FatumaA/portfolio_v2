2eac50279dcfa0377e47a13e30728c714bc7ce9d
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-08-29 14:43:04 +0300

    temp: direct to hashnode subdomain on blog card click

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -30,11 +30,11 @@ function Card(props : cardInfo) {
   const isProj = props.projs !== undefined;
     return (  
       <div className='card'>
-        <a href={isProj ? props.projs!.projectImgUrl : props.blogs!.slug } target='_blank' rel='noreferrer'>
+        <a href={isProj ? props.projs!.projectImgUrl : 'https://blog.hijabicoder.dev/'+props.blogs!.slug} target={isProj ? '_blank': undefined} rel='noreferrer'>
         <Image src={isProj ? props.projs!.projImgSrc : props.blogs!.coverImage} alt='screenshot' width="200" height="80"/> 
-          <h3 className="my-4 font-extrabold"> {isProj ? props.projs!.projStack : props.blogs?.title} </h3>
-          <p  className="mt-2 mb-4"> {isProj ? props.projs!.projDesc : props.blogs?.brief} </p>
-          <a href={isProj ? props.projs!.projCodeUrl : props.blogs?.slug} target={isProj ? '_blank': undefined} rel='noreferrer' className="text-yellow-400 hover:scale-50"> 
+          <h3 className="my-4 font-extrabold"> {isProj ? props.projs!.projStack : props.blogs!.title} </h3>
+          <p  className="mt-2 mb-4"> {isProj ? props.projs!.projDesc : props.blogs!.brief} </p>
+          <a href={isProj ? props.projs!.projCodeUrl : 'https://blog.hijabicoder.dev/'+props.blogs!.slug} target={isProj ? '_blank': undefined} rel='noreferrer' className="text-yellow-400 hover:scale-50"> 
             {isProj ? props.projs!.projTextLink: "Read More"} 
           </a>
         </a>
