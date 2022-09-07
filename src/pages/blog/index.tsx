a5fe002e15b55fb7a2b42da78ff0cd827a65a63c
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-09-07 13:43:48 +0300

    add types

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,18 +1,17 @@
 import React from 'react'
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { gql } from '@apollo/client';
 
-import { NextPage } from 'next';
-import Card from '../../components/Card';
+import Card, { IBlog } from '../../components/Card';
 import { Client, GET_BLOGS } from '../../queries';
-
-const Blog: NextPage = ({posts}: any) => {
+ 
+const Blog = ({posts}: any) => {
     return (
         <div className="top-styles my-14">
             <h5 className="tagline"> BLOG POSTS </h5>
             <h1 className="headline">Thoughts on the tech I&apos;m using, learning and loving </h1>
             <div className="card-section">
-            {posts.map((p: any, index: number ) => 
-             <Card key={index} blogs={p}/>
+            {posts.map((p: IBlog, index: number ) => 
+             <Card key={index} cardInfo={p}/>
 
             )}
 
