da3808bc2b0805d3edd47b34e393739224ccd26d
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-09-07 15:42:38 +0300

    configure redirect to youtube channel

--- next.config.js
+++ next.config.js
@@ -3,5 +3,14 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["cdn.hashnode.com"],
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/videos',
+        destination: 'https://www.youtube.com/c/HijabiCoder/videos',
+        permanent: false,
+      },
+    ]
+  },
 }
