2044610b6720bae6f28d8e8bb636bbd969ea67de
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-03-14 23:51:11 +0300

    fix: remove nested <a> and <p> tags to clear hydration errors

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,37 +1,43 @@
-import Link from 'next/link'
-import { AiFillTwitterSquare, AiFillLinkedin } from 'react-icons/ai';
-import Button from './Button'
-import { AboutInfo, ButtonInfo } from '../Data'
+import Link from "next/link";
+import { AiFillTwitterSquare, AiFillLinkedin } from "react-icons/ai";
+import Button from "./Button";
+import { AboutInfo, ButtonInfo } from "../Data";
 
 const About = () => {
-    return (
-        <div id="about" className='top-styles'>
-            <h5 className="tagline">{AboutInfo.tagLine}</h5>
-            <h1 className="headline">{AboutInfo.headLine}</h1>
-            <p className="text-center leading-7">{AboutInfo.subHeadLine}</p>
-            <p className="text-center leading-7 mt-4">
-                Sounds interesting? Get in touch via the form below and let&apos;s talk. 
-                <p className="text-center leading-7 mt-1"> You can also find me at: </p>
-                <span className='flex flex-row justify-center mt-1'> 
-                    <a href='https://twitter.com/XquisiteDreamer' target='_blank' rel='noreferrer'>
-                        <AiFillTwitterSquare/>
-                    </a> 
-                    <a href='https://www.linkedin.com/in/fatuma-abdullahi-6b804948/' target='_blank' rel='noreferrer'>
-                        <AiFillLinkedin/>
-                    </a> 
-                </span>
-            
-            </p>
+	return (
+		<div id="about" className="top-styles">
+			<h5 className="tagline">{AboutInfo.tagLine}</h5>
+			<h1 className="headline">{AboutInfo.headLine}</h1>
+			<p className="text-center leading-7">{AboutInfo.subHeadLine}</p>
+			<p className="text-center leading-7 mt-4">
+				Sounds interesting? Get in touch via the form below and let&apos;s talk.
+				<span className="text-center leading-7 mt-1">
+					{" "}
+					You can also find me at:{" "}
+				</span>
+				<span className="flex flex-row justify-center mt-1">
+					<a
+						href="https://twitter.com/XquisiteDreamer"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<AiFillTwitterSquare />
+					</a>
+					<a
+						href="https://www.linkedin.com/in/fatuma-abdullahi-6b804948/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<AiFillLinkedin />
+					</a>
+				</span>
+			</p>
 
-        
-            <Link href='/#contacts'>
-                <a>
-                    <Button btnText={ButtonInfo.about}/>
-                </a>     
-            </Link>
+			<Link href="/#contacts">
+				<Button btnText={ButtonInfo.about} />
+			</Link>
+		</div>
+	);
+};
 
-        </div>
-    )
-}
-
-export default About
+export default About;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,90 +1,146 @@
-import React, { useState } from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import {GoThreeBars, GoX} from 'react-icons/go';
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { GoX } from "react-icons/go";
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
-    const [click, setClick] = useState(false);
+	const [click, setClick] = useState(false);
 
-    const handleClick = () => {
-          setClick(!click)
-    }
+	const handleClick = () => {
+		setClick(!click);
+	};
 
-    return (
-        <>
-         {/* Main navigation bar + logo */}
-        <div className='hidden md:block sticky top-10 z-10'>
-            <div className="text-brand-lighter h-4 font-bold flex justify-between items-center mx-24 my-8">
-                <div className='hidden md:block'>
-                    <Link href='/' passHref>
-                    <Image alt="Logo" src="/images/logo.png" width={52} height={52} className="cursor-pointer"></Image>
-                    </Link>
-                </div>
+	return (
+		<>
+			{/* Main navigation bar + logo */}
+			<div className="hidden md:block sticky top-10 z-10">
+				<div className="text-brand-lighter h-4 font-bold flex justify-between items-center mx-24 my-8">
+					<div className="hidden md:block">
+						<Link href="/" passHref>
+							<Image
+								alt="Logo"
+								src="/images/logo.png"
+								width={52}
+								height={52}
+								className="cursor-pointer"
+							></Image>
+						</Link>
+					</div>
 
-                <div className="space-x-4 hidden md:block">
-                <Link href={'/#home'}>
-                    <a className="hover:text-brand-accent">HOME</a>
-                </Link>
-                <Link href={'/#about'}>
-                    <a className="hover:text-brand-accent">ABOUT</a>
-                </Link>
-                <Link href={'/#projects'}>
-                    <a className="hover:text-brand-accent">PROJECTS</a>
-                </Link>
-                <Link href={'/blog'}>
-                    <a className="hover:text-brand-accent">BLOG</a>
-                </Link>
-                {/* <Link href={'/videos'}> */}
-                    <a href='https://www.youtube.com/c/HijabiCoder/videos' target='_blank' rel='noreferrer' className="hover:text-brand-accent">VIDEOS</a>
-                {/* </Link> */}
-                <Link href={'/#contacts'}>
-                    <a className="hover:text-brand-accent">CONTACTS</a>
-                </Link>
-                </div>
-                            
-            </div>
-        </div>
-     
+					<div className="space-x-4 hidden md:block">
+						<Link href="#home" className="hover:text-brand-accent">
+							HOME
+						</Link>
+						<Link href="#about" className="hover:text-brand-accent">
+							ABOUT
+						</Link>
+						<Link href="#projects" className="hover:text-brand-accent">
+							PROJECTS
+						</Link>
+						<Link href={"/blog"} className="hover:text-brand-accent">
+							BLOG
+						</Link>
+						{/* <Link href={'/videos'}> */}
+						<Link
+							href="https://www.youtube.com/c/HijabiCoder/videos"
+							target="_blank"
+							rel="noreferrer"
+							className="hover:text-brand-accent"
+						>
+							VIDEOS
+						</Link>
+						{/* </Link> */}
+						<Link href="#contacts" className="hover:text-brand-accent">
+							CONTACTS
+						</Link>
+					</div>
+				</div>
+			</div>
 
-         {/* Mobile navigation bar + logo */}
+			{/* Mobile navigation bar + logo */}
 
-         <div className="top-0 h-4 sticky z-10 active:bg-bgColor-cardDark p-4 md:hidden">
-            <div className="text-brand-lighter lg:hidden font-bold flex justify-between items-center mx-5 sticky z-10">
-                <Link href='/' passHref>
-                    <Image alt="Logo" src="/images/logo.png" width={35} height={35}  className="cursor-pointer"></Image>
-                </Link>
-                <GoThreeBars style={{display:click ? "none" :  "block"}} onClick={handleClick}/>
-                <GoX style={{display:click ? "block" :  "none"}} onClick={handleClick}/>
-            </div>
+			<div className="top-0 h-4 sticky z-10 active:bg-bgColor-cardDark p-4 md:hidden">
+				<div className="text-brand-lighter lg:hidden font-bold flex justify-between items-center mx-5 sticky z-10">
+					<Link href="/" passHref>
+						<Image
+							alt="Logo"
+							src="/images/logo.png"
+							width={35}
+							height={35}
+							className="cursor-pointer"
+						></Image>
+					</Link>
+					<FaBars
+						style={{ display: click ? "none" : "block" }}
+						onClick={handleClick}
+					/>
+					<GoX
+						style={{ display: click ? "block" : "none" }}
+						onClick={handleClick}
+					/>
+				</div>
 
-            <div style={{display:click ? "block" :  "none"}} className="space-x-4 lg:hidden bg-bgColor-cardDark text-brand-lighter font-bold mx-4 md:mx-16 pt-4 pb-4 z-10">
-                <div className="z-10">
-                    <Link href={'/#home'}>
-                        <a onClick={handleClick} className="block p-4 hover:text-brand-accent hover:bg-brand-hoverDark">HOME</a>
-                    </Link>
-                    <Link href={'/#about'}>
-                        <a onClick={handleClick} className="block p-4 hover:text-brand-accent hover:bg-brand-hoverDark">ABOUT</a>
-                    </Link>
-                    <Link href={'/#projects'}>
-                        <a onClick={handleClick} className="block p-4 hover:text-brand-accent hover:bg-brand-hoverDark">PROJECTS</a>
-                    </Link>
-                    {/* <Link href={'/blog'}> */}
-                        {/* <a onClick={handleClick} className="block p-4 hover:text-brand-accent hover:bg-brand-hoverDark">BLOG</a> */}
-                        <a href="https://blog.hijabicoder.dev/" target='_blank' rel='noreferrer' className="block p-4 hover:text-brand-accent hover:bg-brand-hoverDark">BLOG</a>
-                    {/* </Link> */}
-                    {/* <Link href={'/videos'}> */}
-                        {/* <a onClick={handleClick} className="block p-4 hover:text-brand-accent hover:bg-brand-hoverDark">VIDEOS</a> */}
-                        <a href="https://www.youtube.com/channel/UC2GdvJFxCkQEJCcDZTp-Dmg/videos" target='_blank' rel='noreferrer' className="block p-4 hover:text-brand-accent hover:bg-brand-hoverDark">VIDEOS</a>
-                    {/* </Link> */}
-                    <Link href={'/#contacts'}>
-                        <a onClick={handleClick} className="block p-4 hover:text-brand-accent hover:bg-brand-hoverDark">CONTACTS</a>
-                    </Link>
-                </div>
-            </div>
-         </div>
-        
-         </>
-    )
-}
+				<div
+					style={{ display: click ? "block" : "none" }}
+					className="space-x-4 lg:hidden bg-bgColor-cardDark text-brand-lighter font-bold mx-4 md:mx-16 pt-4 pb-4 z-10"
+				>
+					<div className="z-10">
+						<Link
+							href="#home"
+							onClick={handleClick}
+							className="block p-4 hover:text-brand-accent hover:bg-brand-hoverDark"
+						>
+							HOME
+						</Link>
+						<Link
+							href="#about"
+							onClick={handleClick}
+							className="block p-4 hover:text-brand-accent hover:bg-brand-hoverDark"
+						>
+							ABOUT
+						</Link>
+						<Link
+							href="#projects"
+							onClick={handleClick}
+							className="block p-4 hover:text-brand-accent hover:bg-brand-hoverDark"
+						>
+							PROJECTS
+						</Link>
+						{/* <Link href={'/blog'}> */}
+						{/* <a onClick={handleClick} className="block p-4 hover:text-brand-accent hover:bg-brand-hoverDark">BLOG</a> */}
+						<Link
+							href="https://blog.hijabicoder.dev/"
+							target="_blank"
+							rel="noreferrer"
+							className="block p-4 hover:text-brand-accent hover:bg-brand-hoverDark"
+						>
+							BLOG
+						</Link>
+						{/* </Link> */}
+						{/* <Link href={'/videos'}> */}
+						{/* <a onClick={handleClick} className="block p-4 hover:text-brand-accent hover:bg-brand-hoverDark">VIDEOS</a> */}
+						<Link
+							href="https://www.youtube.com/channel/UC2GdvJFxCkQEJCcDZTp-Dmg/videos"
+							target="_blank"
+							rel="noreferrer"
+							className="block p-4 hover:text-brand-accent hover:bg-brand-hoverDark"
+						>
+							VIDEOS
+						</Link>
+						{/* </Link> */}
+						<Link
+							href="#contacts"
+							onClick={handleClick}
+							className="block p-4 hover:text-brand-accent hover:bg-brand-hoverDark"
+						>
+							CONTACTS
+						</Link>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
 
-export default Navbar
+export default Navbar;
