b1bb940407fafd5840f43c84b3579614c0ec954f
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-03-14 23:51:11 +0300

    fix: remove nested <a> and <p> tags to clear hydration errors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,43 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div className="text-brand-lighter text-center font-bold  mx-24 top-0 sticky pt-4 pb-4">
-            <h3>Delicately crafted by: <span>
-                <Link href="https://twitter.com/XquisiteDreamer">
-                    <a target='_blank' rel='noreferrer'>Fatuma A - HijabiCoder</a>
-                </Link>
-                </span>
-            </h3>
-            <h2>Using <span>
-                <Link href="https://nextjs.org/">
-                    <a target='_blank' rel='noreferrer'>NextJs</a>
-                </Link></span>
-            </h2>
-            <h2>Hosted on <span>
-                <Link href="https://www.netlify.com/">
-                    <a target='_blank' rel='noreferrer'>Netlify</a>
-                </Link>
-                </span>
-            </h2>
+	return (
+		<div className="text-brand-lighter text-center font-bold  mx-24 top-0 sticky pt-4 pb-4">
+			<h3>
+				Delicately crafted by:{" "}
+				<span>
+					<Link
+						href="https://twitter.com/XquisiteDreamer"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Fatuma A - HijabiCoder
+					</Link>
+				</span>
+			</h3>
+			<h2>
+				Using{" "}
+				<span>
+					<Link href="https://nextjs.org/" target="_blank" rel="noreferrer">
+						NextJs
+					</Link>
+				</span>
+			</h2>
+			<h2>
+				Hosted on{" "}
+				<span>
+					<Link
+						href="https://www.netlify.com/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Netlify
+					</Link>
+				</span>
+			</h2>
+		</div>
+	);
+};
 
-        </div>
-    )
-}
-
-export default Footer
+export default Footer;
