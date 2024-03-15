3bfc58388a722ea90b6e1ec47c44179340c1b049
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-03-15 12:25:08 +0300

    temp: remove hashnode cdn

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ["cdn.hashnode.com"],
-  },
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/videos',
-  //       destination: 'https://www.youtube.com/c/HijabiCoder/videos',
-  //       permanent: false,
-  //     },
-  //   ]
-  // },
-}
+	reactStrictMode: true,
+	// images: {
+	//   domains: ["cdn.hashnode.com"],
+	// },
+	// async redirects() {
+	//   return [
+	//     {
+	//       source: '/videos',
+	//       destination: 'https://www.youtube.com/c/HijabiCoder/videos',
+	//       permanent: false,
+	//     },
+	//   ]
+	// },
+};
