e457400b9449342c1ca00bb7422fb9fa9b27103f
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-03-28 13:23:24 +0300

    fix: responsive - remove logos on mobile

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import Image from 'next/image'
-import { BannerInfo } from '../Data'
+import React from "react";
+import Image from "next/image";
+import { BannerInfo } from "../Data";
 
 const Banner = () => {
-    return (
-        <div className="text-white border-t-2 border-b-2 border-brand-accent py-10 flex flex-row justify-between mx-8">
-            { BannerInfo.map((img, index) => (
-                <Image key={index} alt={img} src={img} width={100} height={100}></Image>
-            ))
-            }
-        </div>
-    )
-}
+	return (
+		<div className="hidden md:flex flex-row  text-white border-t-2 border-b-2 border-brand-accent py-10 justify-between mx-8">
+			{BannerInfo.map((img, index) => (
+				<Image key={index} alt={img} src={img} width={100} height={100}></Image>
+			))}
+		</div>
+	);
+};
 
-export default Banner
+export default Banner;
