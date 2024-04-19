a6c643e011db1f6040df582991568df13fae407e
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-04-20 01:45:49 +0300

    chore: update tailwind config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+/** @type {import('postcss-load-config').Config} */
+const config = {
+	plugins: {
+		tailwindcss: {},
+	},
+};
+
+export default config;
