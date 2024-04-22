4c5aa511ede2fe483e9ef6ed54f550c741e66271
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-04-22 15:28:30 +0300

    styles: page and card

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -15,10 +15,10 @@ const Page = async () => {
 	return (
 		<div className="top-styles my-14">
 			<h5 className="tagline"> BLOG POSTS </h5>
-			<h1 className="headline">
+			<h1 className="headline mb-8">
 				Thoughts on the tech I&apos;m using, learning and loving{" "}
 			</h1>
-			<div className="card-section">
+			<div className="card-section w-8/12">
 				{edges.map(({ node }: IBlog, index: number) => (
 					<Card key={index} cardInfo={node} />
 				))}
