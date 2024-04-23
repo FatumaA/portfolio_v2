9f260197deaf1de34cf5b2c292788c3927b1d7e7
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-04-23 23:27:38 +0300

    chore: add a client instance of sb

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-
+// "use server";
+import Search from "@/components/Search";
 import Card, { IBlog } from "../../components/Card";
 import { getClient, getBlogsQuery } from "../../queries";
+import { supabaseServerClient } from "@/lib/supabase";
 
 const Page = async () => {
 	const {
@@ -11,13 +12,84 @@ const Page = async () => {
 			},
 		},
 	} = await getClient().query({ query: getBlogsQuery });
+
+	// const postToSupabase = (edges) => {
+	// edges.forEach(async ({ node }: { node: IBlog }) => {
+	// 	const {
+	// 		title,
+	// 		subtitle,
+	// 		brief,
+	// 		slug,
+	// 		content: { html, markdown },
+	// 		coverImage: { url = "" },
+	// 	} = node;
+
+	// 	const { data, error } = await supabaseServerClient
+	// 		.from("hashnode_blogs")
+	// 		.insert({ title, subtitle, brief, slug, html, markdown, url });
+
+	// 	if (error) {
+	// 		console.error("Error inserting blog to supabase", error.hint);
+	// 		return;
+	// 	}
+
+	// 	console.log("Blog inserted to supabase", data);
+	// 	return data;
+	// });
+	// };
+
+	// const search = async (searchData: FormData) => {
+	// 	"use server";
+	// 	const searchTerm = searchData.get("search")!;
+	// 	const searchString = searchTerm.toString();
+
+	// 	console.log("searching for ", searchString);
+
+	// 	// coz 2 or more words are treated as an exact match
+	// 	const preparedDBSearchString = searchString.split(" ").join(" or ");
+
+	// 	const { data, error } = await supabaseServerClient
+	// 		.from("hashnode_blogs")
+	// 		.select("slug")
+	// 		.textSearch("title", preparedDBSearchString, {
+	// 			type: "websearch",
+	// 			config: "english",
+	// 		});
+
+	// 	const { data: mdData, error: mdError } = await supabaseServerClient
+	// 		.from("hashnode_blogs")
+	// 		.select("slug")
+	// 		.textSearch("markdown", preparedDBSearchString, {
+	// 			type: "websearch",
+	// 			config: "english",
+	// 		});
+
+	// 	searchData.set("search", "");
+
+	// 	console.log("COMBINED SEARCHH", [{ ...data, ...mdData }]);
+	// };
+
 	console.log(edges[4].node.title);
+
 	return (
 		<div className="top-styles my-14">
 			<h5 className="tagline"> BLOG POSTS </h5>
 			<h1 className="headline mb-8">
 				Thoughts on the tech I&apos;m using, learning and loving{" "}
 			</h1>
+			{/* <form action={search} className="mb-8 flex gap-3">
+				<input
+					type="text"
+					placeholder="search..."
+					name="search"
+					className="rounded-sm p-2 bg-inherit w-96 border focus:border-brand-accent focus:outline-brand-accent focus:outline-none"
+				/>
+				<button type="submit" className="btn-primary my-0 w-32">
+					Search
+				</button>
+			</form> */}
+			<Search />
+
 			<div className="card-section w-8/12">
 				{edges.map(({ node }: { node: IBlog }, index: number) => (
 					<Card key={index} cardInfo={node} />
